b38d58f64654f7a7aebec12aaee41157085cb513
WalterPerjenko <komarovilia07@gmail.com>
2026-06-07 18:52:33 +0300

    Delete src/main/kotlin directory

--- src/main/kotlin/ContainerType.java
+++
@@ -1,7 +0,0 @@
-package domain;
-
-public enum ContainerType {
-    FREEZER,
-    FRIDGE,
-    BOX;
-}
--- src/main/kotlin/validators/SlotValidator.java
+++
@@ -1,9 +0,0 @@
-package validators;
-
-public final class SlotValidator {
-    private SlotValidator() {}
-
-    public static void validateDimensions(int rows, int cols) {
-        if (rows <= 0 || cols <= 0) throw new IllegalArgumentException("Ошибка: количество рядов/колонок должно быть > 0");
-    }
-}
